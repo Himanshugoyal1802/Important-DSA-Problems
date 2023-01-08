8ccaea0eb323431448cdd146998de1f1626cae31
Himanshu Goyal <78136098+Himanshugoyal1802@users.noreply.github.com>
2023-01-09 00:23:39 +0530

    Add files via upload

---
+++ subset2.cpp
@@ -0,0 +1,47 @@
+/*
+
+        //Brute Force
+        TC - 2^n * klog(x)
+        SC - 2^n * k to store every subset of avg length k
+
+klog(x) is due to inserting every subset in a set.
+
+At every index, we make a decision whether to pick or not pick the element at that index. 
+This will help us in generating all possible combinations but does not take care of the duplicates. 
+Hence we will use a set to store all the combinations that will discard the duplicates.
+
+*/
+
+
+/*
+
+    //Optimal approach
+    TC - 2^n
+    SC - 2^n * k
+
+
+Code: 
+
+#include <bits/stdc++.h> 
+
+void uniqueSubsetsHelper(int ind, vector<int> &arr, int n, vector<vector<int>> &ans, vector<int> &v){
+    ans.push_back(v);
+    for(int i = ind; i < n; i++){
+        if(i != ind && arr[i] == arr[i-1])continue;
+        v.push_back(arr[i]);
+        uniqueSubsetsHelper(i+1, arr, n, ans, v);
+        v.pop_back();
+    }
+}
+
+vector<vector<int>> uniqueSubsets(int n, vector<int> &arr)
+{
+    int ind = 0;
+    vector<vector<int>> ans;
+    sort(arr.begin(), arr.end());
+    vector<int> v;
+    uniqueSubsetsHelper(ind, arr, n, ans, v);
+    return ans;
+}
+
+*/
