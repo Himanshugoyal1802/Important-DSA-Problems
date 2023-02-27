09564d18cf768b986032b874d15d4b0b2745f040
Himanshu Goyal <78136098+Himanshugoyal1802@users.noreply.github.com>
2023-02-27 22:08:31 +0530

    Add files via upload

---
+++ preInPostOrderTraversalInOneTraversal.cpp
@@ -0,0 +1,68 @@
+/*
+
+        TC - 3N
+        SC  - N
+    
+Code:
+
+vector<vector<int>> getTreeTraversal(BinaryTreeNode<int> *root){
+    vector<vector<int>> rslt;
+    vector<int>pre, in ,post;
+
+    stack < pair < BinaryTreeNode<int> * , int >> st;
+    st.push({
+        root,
+        1
+    });
+    if (root == NULL) return rslt;
+
+    while (!st.empty()) {
+        auto it = st.top();
+        st.pop();
+
+        // this is part of pre
+        // increment 1 to 2 
+        // push the left side of the tree
+        if (it.second == 1) {
+        pre.push_back(it.first -> data);
+        it.second++;
+        st.push(it);
+
+        if (it.first -> left != NULL) {
+            st.push({
+            it.first -> left,
+            1
+            });
+        }
+        }
+
+        // this is a part of in 
+        // increment 2 to 3 
+        // push right 
+        else if (it.second == 2) {
+        in .push_back(it.first -> data);
+        it.second++;
+        st.push(it);
+
+        if (it.first -> right != NULL) {
+            st.push({
+            it.first -> right,
+            1
+            });
+        }
+        }
+        // don't push it back again
+        else {
+        post.push_back(it.first->data);
+        }
+    }
+
+    rslt.push_back(in);
+        rslt.push_back(pre);
+    rslt.push_back(post);
+
+    return rslt;
+}
+
+
+*/
